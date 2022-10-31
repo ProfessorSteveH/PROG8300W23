ca998ee3e23f46b606a85194aa79e9a1bf49df44
steve <steve@redacted.com>
2022-10-31 14:22:16 -0400

    v5.4

--- dockers/owncloud/docker-start.sh
+++ dockers/owncloud/docker-start.sh
@@ -7,6 +7,15 @@ else
     mkdir $HOME/prog8300 
 fi
 
-export EXT_IP=$(ip route get 8.8.8.8 | awk -F"src " 'NR==1{split($2,a," ");print a[1]}')
+ip route get 8.8.8.8 > /dev/null 2>&1
+if [ $? -eq 0 ]
+then
+	echo "on routable IP network"
+	export EXT_IP=$(ip route get 8.8.8.8 | awk -F"src " 'NR==1{split($2,a," ");print a[1]}')
+else
+	echo "on host-only network, using eth0 device for primary IP"
+	export EXT_IP=$(ip route | grep eth0 | awk -F"src " 'NR==1{split($2,a," ");print a[1]}')
+fi
+
 docker run --name owncloud --detach   -e EXTERNAL_IP=$EXT_IP -v "$HOME/prog8300:/mnt/data/files:cached" -p 80:80 owncloud
 
